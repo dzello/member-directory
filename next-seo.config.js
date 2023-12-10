960de1c5e00ffa0d9f3e943e221cccd1e3365b32
Josh Dzielak <jdzielak@gmail.com>
2023-12-10 11:30:18 +0100

    Fix SEO

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,20 +2,20 @@ export default {
   defaultTitle: "DM Dinner Club Member Directory | Powered by Orbit",
   description:
     "Discover who's attending the DM Dinner Club. Connect, network, and engage with attendees, and explore their social profiles all in one place.",
-  canonical: "https://www.members.dmdinner.club/",
+  canonical: "https://dmdinner.club/",
   twitter: {
     cardType: "summary",
     handle: "@RoiStartup",
   },
   openGraph: {
     type: "website",
-    url: "https://www.members.dmdinner.club/",
+    url: "https://dmdinner.club/",
     title: "DM Dinner Club Member Directory | Powered by Orbit",
     description:
       "Discover who's attending the DM Dinner Club. Connect, network, and engage with attendees, and explore their social profiles all in one place.",
     images: [
       {
-        url: `https://member-directory-eight.vercel.app/brand-logo-background.png`,
+        url: `https://dmdinner.club/brand-logo-background.png`,
         width: 200,
         height: 200,
         alt: "DM Dinner Club - Established 2022",
@@ -25,7 +25,7 @@ export default {
   additionalLinkTags: [
     {
       rel: "icon",
-      href: `https://member-directory-eight.vercel.app/brand-logo-background.png`,
+      href: `https://dmdinner.club/brand-logo-background.png`,
     },
   ],
   additionalMetaTags: [
